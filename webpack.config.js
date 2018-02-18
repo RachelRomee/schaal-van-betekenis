d9ee1d1579a9e909231ababd1578b7a30fd6b02e
RachelRomee <rachelromee@icloud.com>
2018-02-18 18:17:04 +0100

    clean webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 var webpackConfig = {
 	entry: "./assets/js/index.js", // entry page - if multiple it's an object
 	output: { // files that you will deploy
-		// path: "public", // which folder to place bundle in
-		path: path.resolve(__dirname, 'public'),
+		path: path.resolve(__dirname, 'public'), // which folder to place bundle in
 		filename: "bundle.js" // name of bundled file
 	},
 	module: {
@@ -16,12 +15,23 @@ var webpackConfig = {
 			},
 			{
 				loaders: ["style-loader", 'css-loader', 'sass-loader'],
-				test: /\.scss$/
+				 test:/\.(s*)css$/,
 			},
+			// {
+			// 	loaders: ["url-loader", "img-loader"],
+			// 	test: /\.png$/
+			// },
 			{
-				loaders: ["url-loader", "img-loader"],
-				test: /\.png$/
-			}
+            test: /\.(png|jp(e*)g|gif|svg)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 8000, // Convert images < 8kb to base64 strings
+                    name: 'img/[hash]-[name].[ext]'
+                }
+            }]
+        }
+
 		]
 	},
 	plugins: [
@@ -32,3 +42,12 @@ var webpackConfig = {
 };
 
 module.exports = webpackConfig;
+
+
+
+/**
+// @TODO
+// autoprefix css: https://github.com/passy/autoprefixer-loader - already installed npm package
+// images without relative path (=absolute)
+// compress images: https://github.com/tcoopman/image-webpack-loader
+*/

c05f2ac36993d1e15c818f7711d352cc44a8a8af
RachelRomee <rachelromee@icloud.com>
2018-02-18 18:46:29 +0100

    add js components folder + globalfunctions file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,16 @@
+/* IMPORT STYLES
+------------------------------------------------ */
 import "../styles/base.scss";
+
+
+/* IMPORT COMPONENTS
+------------------------------------------------ */
+import { test } from "./components/test";
+import { globalFunctions } from "./globalFunctions";
+
+test();
+console.log(globalFunctions);
+
 
 console.log('you are awesome');
 console.log('rachel is lief');
